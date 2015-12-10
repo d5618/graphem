efdeb2faac90ad6c929b4b50a9c797c643cbcaee
Dominique Guillot <dguillot@stanford.edu>
2015-12-10 14:08:25 -0500

    Added missing files; Replaced huge_glasso by QUIC in Graphical_lasso.m

--- graphical_lasso.m
+++ graphical_lasso.m
@@ -1,12 +1,18 @@
-function [Sigma,Omega] = GraphicalLasso(S,lambda,varargin)
+function [Sigma,Omega] = GraphicalLasso(S,lambda)
 
-    switch nargin
-        case 2
-            [Sigma,Omega] = huge_glasso(S,lambda); 
-        case 3 
-            [Sigma, Omega] = huge_glasso(S,lambda,varargin{1});
-        case 4 
-            [Sigma, Omega] = huge_glasso(S,lambda,varargin{1}, varargin{2});
-    end
+    p = size(S,1);
+    Sigma = zeros(p,p);
+    Omega = zeros(p,p);
+    pen = lambda*ones(p,p);
 
+    [Omega Sigma opt cputime niter dGap] = QUIC('default', S, pen, 1e-3, 0, 1000, Omega, Sigma);
+
+    %switch nargin
+    %    case 2
+    %        [Sigma,Omega] = huge_glasso(S,lambda); 
+    %    case 3 
+    %        [Sigma, Omega] = huge_glasso(S,lambda,varargin{1});
+    %    case 4 
+    %        [Sigma, Omega] = huge_glasso(S,lambda,varargin{1}, varargin{2});
+    %end
 end
